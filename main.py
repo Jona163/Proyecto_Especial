7dde0163689b319600e5cdaa032203b3360e574b
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-17 22:58:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+from flask import Flask, render_template, request
+import os
+import importlib
+import pandas as pd
+import joblib
+from flask import render_template
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
